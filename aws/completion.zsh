0b673a2486abc868b5bf050ab7509dfc3b3d9dad
fowlerd1 <43857703+fowlerd1@users.noreply.github.com>
2024-04-17 11:08:03 +0100

    Update completion.zsh

--- aws/completion.zsh
+++ aws/completion.zsh
@@ -1,4 +1,2 @@
-#!/bin/sh
-# shellcheck disable=SC1090,SC1091
-test -e /usr/local/bin/aws_zsh_completer.sh &&
-	. /usr/local/bin/aws_zsh_completer.sh
+#!/bin/zsh
+fpath=(/opt/homebrew/bin/aws_completer $fpath)
